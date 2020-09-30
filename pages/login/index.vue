783697143d77b373390675962cfd5d2264dfd9fa
AgustinOsses <ossesagus@gmail.com>
2020-09-30 09:56:10 -0300

    The child components of the login were generated

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,56 +1,6 @@
 
-
-
 <template>
-  <a-row class="container" type="flex" justify="center">
-    <a-col class="form">
-      <a-form-model
-        class="form__model"
-        layout="vertical"
-        :model="userForm"
-        @submit="handleSubmit"
-        @submit.native.prevent
-      >
-        <a-form-model-item>
-          <a-input v-model.trim="userForm.user" placeholder="Usuario">
-            <a-icon
-              slot="prefix"
-              type="user"
-              style="color: rgba(0, 0, 0, 0.25)"
-            />
-          </a-input>
-        </a-form-model-item>
-        <a-form-model-item>
-          <a-input-password
-            v-model="userForm.password"
-            type="password"
-            placeholder="Contraseña"
-          >
-            <a-icon
-              slot="prefix"
-              type="lock"
-              style="color: rgba(0, 0, 0, 0.25)"
-            />
-          </a-input-password>
-        </a-form-model-item>
-        <a-form-model-item>
-          <a-button
-            type="primary"
-            block
-            html-type="submit"
-            :disabled="userForm.user === '' || userForm.password === ''"
-          >
-            Ingresar
-          </a-button>
-          <a-row type="flex" justify="center">
-            <a-col>
-              <a>¿Olvidaste tu contraseña?</a>
-            </a-col>
-          </a-row>
-        </a-form-model-item>
-      </a-form-model>
-    </a-col>
-  </a-row>
+  <login> </login>
 </template>
 
 
@@ -65,7 +15,7 @@ export default {
     };
   },
   methods: {
-    handleSubmit() {
+    formSubmit() {
       console.log(this.userForm);
     },
   },
@@ -73,13 +23,14 @@ export default {
 </script>
 
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
 
 .container{
   display:flex ;
   align-items: center;
   width: 100%;
   height: 100%;
+  
 }
 
 .form{
@@ -90,13 +41,8 @@ export default {
   width: 25%;
   background-color: #001529;
   border-radius: 10px;
-  
+  box-shadow: 2px 2px 10px 1px black
 }
-
-
-
-
-
 
 
 </style>
