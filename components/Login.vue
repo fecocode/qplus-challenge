62b0d09fc7eda88e528a89797342661aa6f5913f
fedeperaltx <fedeperaltag@gmail.com>
2020-10-01 12:40:14 -0300

    Login and PasswordRecovery changes

--- components/Login.vue
+++ components/Login.vue
@@ -9,19 +9,12 @@
     >
       <a-form-model-item class="form-model__logo">
         <a-col class="form-model__logo--img-logo">
-          Logo
-        </a-col>
-      </a-form-model-item>
-
-      <a-form-model-item>
-        <a-col class="form-model__tabs">
-          <a href="#">Login</a>
-          <a href="#">Singup</a>
+          <img :src = "require('../assets/svg/logo-zifos.svg')" alt='Logo Zifos'>
         </a-col>
       </a-form-model-item>
 
       <a-form-model-item class="form-model__user">
-        <a-input v-model.trim="userForm.user" placeholder="User">
+        <a-input class="form-model__user--btn-user" v-model.trim="userForm.user" placeholder="Usuario">
           <a-icon
             slot="prefix"
             type="user"
@@ -34,7 +27,7 @@
         <a-input-password
           v-model="userForm.password"
           type="password"
-          placeholder="Password"
+          placeholder="Contraseña"
         >
           <a-icon
             slot="prefix"
@@ -44,29 +37,19 @@
         </a-input-password>
       </a-form-model-item>
 
-      <a-form-model-item class="form-model__dates">
-        <a-row>
-          <a-col>
-            <a-checkbox @change="onChange" class="form-model__checkbox">
-              <span>Remember me</span>
-            </a-checkbox>
-            <a class="form-model__forgot" href="#">¿Forgot password?</a>
-          </a-col>
-        </a-row>
-      </a-form-model-item>
-
       <a-form-model-item class="form-model__submit">
         <a-button
           class="form-model__submit--btn"
-          type="primary"
+          type="danger"
           block
           html-type="submit"
           :disabled="userForm.user === '' || userForm.password === ''"
         >
           Ingresar
         </a-button>
-        <a-row type="flex" justify="center">
-          <a-col>
+
+        <a-row>
+          <a-col class="form-model__forgot">
             <a @click="recovery">¿Olvidaste tu contraseña?</a>
           </a-col>
         </a-row>
@@ -106,81 +89,40 @@ export default {
   padding: 0;
 }
 
-.form {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 80%;
-  width: 35%;
-  background-color: #E8BB51;
-  border-radius: 10px;
-  box-shadow: 2px 2px 8px 1px black;
-  .form-model {
-    &__logo {
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      &--img-logo {
-        width: 3.125rem;
-        height: 3.125rem;
-        margin-top: 0.5rem;
-      }
+.form-model {
+  &__logo {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 3.5rem;
+  }
+  &__user {
+    margin-top: 0.15rem;
+    ::placeholder {
+      padding-left: 4px;
+      font-size: 14px;
+      color: rgba(0, 0, 0, 0.40);
     }
-    &__tabs {
-      display: flex;
-      justify-content: space-around;
-      margin-top: 1rem;
-      margin-bottom: 0.25rem;
-      a {
-        color: rgba(0, 0, 0, 0.60);
-        font-weight: bold;
-        font-size: 14px;
-        &:hover {
-          color: #000000;
-        }
-      }
+  }
+  &__password {
+    margin-top: 0.15rem;
+    ::placeholder {
+      padding-left: 4px;
+      font-size: 14px;
+      color: rgba(0, 0, 0, 0.40);
     }
-    &__user {
-      margin-top: 0.25rem;
-      ::placeholder {
-        padding-left: 4px;
-        font-size: 14px;
-        color: rgba(0, 0, 0, 0.40);
-      }
-    }
-    &__password {
-      margin-top: 0.25rem;
-      ::placeholder {
-        padding-left: 4px;
-        font-size: 14px;
-        color: rgba(0, 0, 0, 0.40);
-      }
-    }
-    &__submit {
-      width: 100%;
-      margin-top: 0.50rem;
-      display: flex;
-      justify-content: center;
-      &--btn {
-        width: 6.25rem;
-      }
-    }
-    &__dates {
-      display: flex;
-      justify-content: space-between;
-      span {
-        font-size: 0.75rem;
-        color: rgba(0, 0, 0, 0.50);
-        &:hover {
-          color: #000000;
-        }
-      }
-      a {
-        font-size: 0.75rem;
-        color: rgba(0, 0, 0, 0.50);
-        &:hover {
-          color: #000000;
-        }
+  }
+  &__submit {
+    width: 100%;
+    margin-top: 0.75rem;
+  }
+  &__forgot {
+    display: flex;
+    justify-content: center;
+    margin-top: 0.50rem;
+    a {
+      color: rgba(237, 31, 44, 0.5);
+      &:hover {
+        color: #ED1F2C;
       }
     }
   }
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="form-container">
-    <div>
-      <login v-if="recovery == false" @click="handleClick"></login>
-      <PasswordRecovery v-else @click="handleClick"></PasswordRecovery>
+  <div class="container">
+    <div class="form-container">
+      <div>
+        <login v-if="recovery == false" @click="handleClick"></login>
+        <PasswordRecovery v-else @click="handleClick"></PasswordRecovery>
+      </div>
     </div>
   </div>
 </template>
@@ -29,14 +31,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.container {
+  width: 100%;
+  height: 100%;
+  background-color: #3D4159;
+}
+
 .form-container {
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 60%;
-  width: 25%;
-  background-color: #001529;
-  border-radius: 10px;
-  box-shadow: 2px 2px 10px 1px black;
+  height: 70%;
+  width: 27%;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #F6E8D9;
+  border-radius: 2rem;
+  margin-top: 7%;
+  box-shadow: 2px 2px 3px 3px rgba(0, 0, 0, 0.3);
 }
+
+
 </style>
